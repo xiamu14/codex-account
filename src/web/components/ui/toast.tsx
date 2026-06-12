45500fb298063a76eaff6d9607fe7642f3bfe3ae
Ben <x_z_gfree@163.com>
2026-06-13 07:44:18 +0800

    fix: keep only one toast visible

--- src/web/components/ui/toast.tsx
+++ src/web/components/ui/toast.tsx
@@ -1,17 +1,23 @@
 // AlignUI Toast v0.0.0
 import * as React from "react";
-import { toast as sonnerToast, Toaster, type ToasterProps } from "sonner";
+import {
+  toast as sonnerToast,
+  Toaster,
+  type ExternalToast,
+} from "sonner";
 
-const defaultOptions: ToasterProps = {
+const singleToastId = "cxa-single-toast";
+
+const defaultOptions: ExternalToast = {
   className: "group/toast z-50",
   position: "bottom-right",
 };
 
 const customToast = (
   renderFunc: (t: string | number) => React.ReactElement,
-  options: ToasterProps = {},
+  options: ExternalToast = {},
 ) => {
-  const mergedOptions = { ...defaultOptions, ...options };
+  const mergedOptions = { ...defaultOptions, ...options, id: singleToastId };
   return sonnerToast.custom(renderFunc, mergedOptions);
 };
 
